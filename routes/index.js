f69a68ec4bae22832c120557965074d6605cb500
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-11-29 00:53:03 +0200

    t

--- routes/index.js
+++ routes/index.js
@@ -124,7 +124,7 @@ function _runTests(testNumber, maxTestsNumber, output, cb) {
         let isValgrindFailureResult = isValgrindFailure(tempLogName);
         let valgrindMessage = "";
         if (isValgrindFailureResult >= 1) {
-            valgrindMessage = "Valgrind has found " + isValgrindFailureResult + " errors), check full output file";
+            valgrindMessage = "Valgrind has found " + isValgrindFailureResult + " error(s), check full output file";
         } else if (isValgrindFailureResult === "UNKNOWN") {
             valgrindMessage = "Valgrind status unknown, please look manually at output file";
         }
