63e154555adfaebd639840b6c1c8834fec3c3b4e
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-09 23:26:55 +0200

    t

--- routes/index.js
+++ routes/index.js
@@ -164,7 +164,7 @@ function _runTests(testNumber, maxTestsNumber, stagingId, output, cb) {
 
 function runTests(stagingId, cb) {
     let tempLogName = `valgrind-test-${makeid(15)}.out.txt`;
-    const EXEC_TEST_NUMBER = `valgrind --leak-check=full --show-leak-kinds=all --log-file="../../public/${tempLogName}" ./compiled_program`;
+    const EXEC_TEST_NUMBER = `cpulimit -l 40 -- valgrind --leak-check=full --show-leak-kinds=all --log-file="../../public/${tempLogName}" ./compiled_program`;
     exec(EXEC_TEST_NUMBER, {timeout: (1000 * 10), cwd: `./staging/${stagingId}`}, function (error, stdout, stderr) {
         let isValgrindFailureResult = isValgrindFailure(tempLogName);
         let valgrindMessage = "";
