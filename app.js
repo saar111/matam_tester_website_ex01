e5ce716b019a31eff86eb6a1c74ace7cb7744057
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-18 17:38:19 +0200

    t

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const banned_ips = ["79.181.105.200"];
+const banned_ips = [];
 
 app.use(ipfilter(banned_ips));
 app.use(logger('common'));
