03226cf376fce2376f12595a69492d1d3b5440f0
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-27 01:18:48 +0200

    t

--- helpers/test_counter.js
+++ helpers/test_counter.js
@@ -1,15 +1,24 @@
 const db = require("./db").getConnection();
+const { MongoClient } = require("mongodb");
 
 
 exports.setLocalsTestCount = function (req, res, next) {
-    db.collection("test_counter").findOne({}, null, (err, test_count) => {
-        res.locals.test_count = test_count.test_count || 0;
-        next();
+    MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true}, (err, client) => {
+        assert.strictEqual(null, err);
+        let _db = client.db("test_counter");
+        _db.collection("test_counter").findOne({}, null, (err, test_count) => {
+            res.locals.test_count = test_count.test_count || 0;
+            next();
+        });
     });
 }
 
 exports.add1ToTestCount = function (req, res, next) {
-    db.collection("test_counter").updateOne({}, {$inc: {"test_count": 1}}, (err, test_count) => {
+    MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true}, (err, client) => {
+        assert.strictEqual(null, err);
+        let _db = client.db("test_counter");
+        _db.collection("test_counter").updateOne({}, {$inc: {"test_count": 1}}, (err, test_count) => {
+        });
+        next();
     });
-    next();
 }
