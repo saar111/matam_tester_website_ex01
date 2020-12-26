875f63b117644ebd2ee01479f1cc2e6b7ccbfbec
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-27 01:32:48 +0200

    t

--- helpers/test_counter.js
+++ helpers/test_counter.js
@@ -1,22 +1,37 @@
-const db = require("./db").getConnection();
-const { MongoClient } = require("mongodb");
+const {MongoClient} = require("mongodb");
 
-
-exports.setLocalsTestCount = function (req, res, next) {
+function getTestCounter(cb) {
     MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true}, (err, client) => {
         let _db = client.db("test_counter");
         _db.collection("test_counter").findOne({}, null, (err, test_count) => {
-            res.locals.test_count = test_count.test_count || 0;
-            next();
+            cb(test_count);
         });
+    });
+}
+
+exports.setLocalsTestCount = function (req, res, next) {
+    getTestCounter((test_count) => {
+        res.locals.test_count = test_count.test_count || 0;
+        res.locals.runners = test_count.runners || 0;
+        next();
     });
 }
 
 exports.add1ToTestCount = function (req, res, next) {
     MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true}, (err, client) => {
-        let _db = client.db("test_counter");
-        _db.collection("test_counter").updateOne({}, {$inc: {"test_count": 1}}, (err, test_count) => {
+        getTestCounter((test_count) => {
+            let runners = test_count.runners;
+            runners.append(req.query.name);
+            runners = Array.from(new Set(runners));
+
+            let _db = client.db("test_counter");
+            _db.collection("test_counter").updateOne({}, {
+                $inc: {"test_count": 1},
+                $set: {runners: runners}
+            }, (err, test_count) => {
+            });
         });
+
         next();
     });
 }
