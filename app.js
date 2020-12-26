889335c1188a1b125118124176064c945c8e6b8b
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-27 01:16:09 +0200

    t

--- app.js
+++ app.js
@@ -17,33 +17,33 @@ app.set('view engine', 'ejs');
 
 const banned_ips = [];
 
-db.connect(() => {});
+db.connect(() => {
 
-app.use(ipfilter(banned_ips));
-app.use(logger('common'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use("/staging", express.static(path.join(__dirname, 'staging')));
-app.use("/ex02", express.static(path.join(__dirname, 'ex02')));
+    app.use(ipfilter(banned_ips));
+    app.use(logger('common'));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: false}));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use("/staging", express.static(path.join(__dirname, 'staging')));
+    app.use("/ex02", express.static(path.join(__dirname, 'ex02')));
 
-app.use('/', indexRouter);
+    app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    app.use(function (req, res, next) {
+        next(createError(404));
+    });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    app.use(function (err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
 });
-
 module.exports = app;
