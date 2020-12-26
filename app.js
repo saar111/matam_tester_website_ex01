a10fbf978ed4c032f2f2c92cd2ab3a9ea07775cc
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-27 01:10:03 +0200

    t

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ipfilter = require('express-ipfilter').IpFilter;
+const db = require("helpers/db");
 
 var indexRouter = require('./routes/index');
 
