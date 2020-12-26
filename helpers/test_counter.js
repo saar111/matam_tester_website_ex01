f9a0e0804c6a24e1259872dbaf833c8b0f42b1e0
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-27 01:13:33 +0200

    t

--- helpers/test_counter.js
+++ helpers/test_counter.js
@@ -1,8 +1,7 @@
-const db = require("./db")
+const db = require("./db").getConnection();
 
 
 exports.setLocalsTestCount = function (req, res, next) {
-    let db = db.getConnection();
     db.collection("test_counter").findOne({}, null, (err, test_count) => {
         res.locals.test_count = test_count.test_count || 0;
         next();
@@ -10,7 +9,6 @@ exports.setLocalsTestCount = function (req, res, next) {
 }
 
 exports.add1ToTestCount = function (req, res, next) {
-    let db = db.getConnection();
     db.collection("test_counter").updateOne({}, {$inc: {"test_count": 1}}, (err, test_count) => {
     });
     next();
