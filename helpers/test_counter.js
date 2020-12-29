02499dd65434b9bf89ef5cf116efb6e3043bf5a7
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-29 20:17:42 +0200

    :try

--- helpers/test_counter.js
+++ helpers/test_counter.js
@@ -3,10 +3,14 @@ const {MongoClient} = require("mongodb");
 function getTestCounter(cb) {
     try {
     MongoClient.connect("mongodb://localhost:27017/", {useNewUrlParser: true}, (err, client) => {
-        let _db = client.db("test_counter");
-        _db.collection("test_counter").findOne({}, null, (err, test_count) => {
-            cb(test_count);
-        });
+		try {
+			let _db = client.db("test_counter");
+			_db.collection("test_counter").findOne({}, null, (err, test_count) => {
+				cb(test_count);
+			});
+		} catch(err) {
+			cb({test_count: 0, runners: []});	
+		}
     });
     } catch (err) {
         cb({test_count: 0, runners: []});
